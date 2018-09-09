449eda038b4cb44d117476220e8d07a1de36d9ec
kwamamissah@gmail.com <kwamamissah@gmail.com>
2018-09-09 14:30:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {artistName: 'songTitles'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = 'songTitle';
+  playlist['artistName'] = 'songTitle';
   
   return playlist;
 }
